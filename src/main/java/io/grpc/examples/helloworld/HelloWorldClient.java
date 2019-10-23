5f3c37d22c4913e1f324931f5e71743a8f814cb4
Eric Gribkoff <ericgribkoff@google.com>
2019-10-23 20:29:15 +0000

    remote peer

--- src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -16,12 +16,25 @@
 
 package io.grpc.examples.helloworld;
 
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ClientInterceptors;
+import io.grpc.Grpc;
 import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.MethodDescriptor;
 import io.grpc.StatusRuntimeException;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.SSLException;
 
 /**
  * A simple client that requests a greeting from the {@link HelloWorldServer}.
@@ -29,31 +42,47 @@ import java.util.logging.Logger;
 public class HelloWorldClient {
   private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
 
-  private final ManagedChannel channel;
+  private final Channel channel;
+  private final ManagedChannel managedChannel;
   private final GreeterGrpc.GreeterBlockingStub blockingStub;
+  private final HeaderClientInterceptor interceptor;
 
   /** Construct client connecting to HelloWorld server at {@code host:port}. */
-  public HelloWorldClient(String host, int port) {
-    this(ManagedChannelBuilder.forAddress(host, port)
-        // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
-        // needing certificates.
-        .usePlaintext()
+  public HelloWorldClient(String target, SslContext sslContext) {
+    this(NettyChannelBuilder.forTarget(target)
+                        .overrideAuthority("foo.test.google.fr")  /* Only for using provided test certs. */
+                                        .sslContext(sslContext)
         .build());
   }
 
   /** Construct client for accessing HelloWorld server using the existing channel. */
-  HelloWorldClient(ManagedChannel channel) {
-    this.channel = channel;
+  HelloWorldClient(ManagedChannel originChannel) {
+    managedChannel = originChannel;
+    interceptor = new HeaderClientInterceptor();
+    channel = ClientInterceptors.intercept(originChannel, interceptor);
     blockingStub = GreeterGrpc.newBlockingStub(channel);
   }
 
+    private static SslContext buildSslContext(String trustCertCollectionFilePath,
+                                              String clientCertChainFilePath,
+                                              String clientPrivateKeyFilePath) throws SSLException {
+        SslContextBuilder builder = GrpcSslContexts.forClient();
+        if (trustCertCollectionFilePath != null) {
+            builder.trustManager(new File(trustCertCollectionFilePath));
+        }
+        if (clientCertChainFilePath != null && clientPrivateKeyFilePath != null) {
+            builder.keyManager(new File(clientCertChainFilePath), new File(clientPrivateKeyFilePath));
+        }
+        return builder.build();
+    }
+
   public void shutdown() throws InterruptedException {
-    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    managedChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
   }
 
   /** Say hello to server. */
   public void greet(String name) {
-    logger.info("Will try to greet " + name + " ...");
+    //logger.info("Will try to greet " + name + " ...");
     HelloRequest request = HelloRequest.newBuilder().setName(name).build();
     HelloReply response;
     try {
@@ -62,7 +91,7 @@ public class HelloWorldClient {
       logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
       return;
     }
-    logger.info("Greeting: " + response.getMessage());
+    System.out.println("Greeting: " + response.getMessage() + ", from " + interceptor.callRef.get().getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR));
   }
 
   /**
@@ -70,17 +99,31 @@ public class HelloWorldClient {
    * greeting.
    */
   public static void main(String[] args) throws Exception {
-    // Access a service running on the local machine on port 50051
-    HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+    String sslCertFile = null;
+    if (args.length == 3) {
+      sslCertFile = args[2];
+    }
+    HelloWorldClient client = new HelloWorldClient(args[0], buildSslContext(sslCertFile, null, null));
+    int secRemaining = Integer.parseInt(args[1]);
     try {
-      String user = "world";
-      // Use the arg as the name to greet if provided
-      if (args.length > 0) {
-        user = args[0];
+      while (secRemaining-- > 0) {
+        String user = "world";
+        client.greet(user);
+        TimeUnit.SECONDS.sleep(1);
       }
-      client.greet(user);
     } finally {
-      client.shutdown();
+        client.shutdown();
     }
   }
+
+ private static class HeaderClientInterceptor implements ClientInterceptor {
+  private final AtomicReference<ClientCall<?, ?>> callRef = new AtomicReference<ClientCall<?, ?>>();
+  @Override
+  public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method,
+      CallOptions callOptions, Channel next) {
+    ClientCall<ReqT, RespT> call = next.newCall(method, callOptions);
+    callRef.set(call);
+    return call;
+  }
+}
 }
